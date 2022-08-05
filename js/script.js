6cf452cb37af8f14315b830509dab8b5b8053bae
arunprasad21 <96112820+arunprasad21@users.noreply.github.com>
2022-08-05 12:29:38 +0530

    Landing Animation

--- js/script.js
+++ js/script.js
@@ -135,9 +135,9 @@ const disableDarkMode = () => {
 
 // If the user already visited and enabled darkMode
 // start things off with it on
-if (darkMode === "enabled") {
-  enableDarkMode();
-}
+// if (darkMode === "enabled") {
+//   enableDarkMode();
+// }
 // When someone clicks the button
 darkModeToggle.addEventListener("click", () => {
   // get their darkMode setting
